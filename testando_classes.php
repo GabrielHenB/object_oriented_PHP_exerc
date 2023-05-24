f9f3af4e84164b5d9e74a5248ef8fed8ad65480d
Gabriel <96265201+GabrielHenB@users.noreply.github.com>
2023-05-24 14:27:07 -0300

    Update testando_classes.php

--- testando_classes.php
+++ testando_classes.php
@@ -2,14 +2,14 @@
 require __DIR__ . "/source/autoload.php";
 
 //Autoloader carrega automaticamente
-$usuario = new \Source\Interp\Usuario("Kazuha");
-$usuarioQueSeraDestruido = new \Source\Interp\Usuario("Aether");
+$usuario = new \Source\Accounts\Usuario("Kazuha");
+$usuarioQueSeraDestruido = new \Source\Accounts\Usuario("Aether");
 
 $usuarioQueSeraDestruido = null; //destruicao
 
 echo "\n Intervalo \n";
 
-$apotheosis = new \Source\Interp\Manga();
+$apotheosis = new \Source\Accounts\Manga();
 $apotheosis->handler("Apotheosis", 14, 1080);
 $apotheosis->rate = 4.5; //A propriedade rate nao existe e desse jeito seria criada
 
